a5b4ddee0b8e30617d50dad8126f095b061f8b92
guilherme-bakaukas <guilherme.tezoli@hotmail.com>
2021-11-10 20:05:58 -0300

    updating previa structure

--- previa/src/dataManagment.py
+++ previa/src/dataManagment.py
@@ -18,23 +18,23 @@ def handleCountryNameIDH(country):
 def handleCountryNameCountry(country):
     return country[:-1]
 
-country_data = pd.read_csv('data/countries of the world.csv')
+country_data = pd.read_csv('../data/external/countries of the world.csv')
 country_data = country_data[['Country','Region','Climate']]
 country_data["Country"] = country_data["Country"].map(handleCountryNameCountry)
 
-IDH_data = pd.read_csv("data/Human Development Index.csv")
+IDH_data = pd.read_csv("../data/external/Human Development Index.csv")
 IDH_data.drop( ["2017"], axis = 1, inplace = True ) #retiramos a coluna de 2017 pois ela não será usada na análise
 IDH_data["Country"] = IDH_data["Country"].map(handleCountryNameIDH)
 IDH_data.rename({'Country':'country'}, axis=1, inplace=True)
 
-sanitation_data = pd.read_csv('data/sanitation_data_2000-2016.csv')
+sanitation_data = pd.read_csv('../data/external/sanitation_data_2000-2016.csv')
 sanitation_data["REF_AREA:Geographic area"] = sanitation_data["REF_AREA:Geographic area"].map(handleCountryNameSanitation)
 sanitation_data["INDICATOR:Indicator"] = sanitation_data["INDICATOR:Indicator"].map(handleIndicators)
 sanitation_data.drop( ["DATAFLOW", "SEX:Sex"], axis = 1, inplace = True )
 sanitation_data = sanitation_data.loc[:, 'REF_AREA:Geographic area':'OBS_VALUE:Observation Value']
 sanitation_data.rename({'REF_AREA:Geographic area': 'country', 'INDICATOR:Indicator':'Indicator', 'TIME_PERIOD:Time period':'year', 'OBS_VALUE:Observation Value':'OBS_VALUE'}, axis=1, inplace=True)
 
-mortality_data = pd.read_excel('data/global_mortality.xlsx')
+mortality_data = pd.read_excel('../data/external/global_mortality.xlsx')
 
 
 for country in IDH_data["country"]:
@@ -64,10 +64,10 @@ IDH_data = newIDH_data[newIDH_data.year.str.contains("index") == False]
 
 IDH_data.dropna(subset = ["HDI_VALUE"], inplace=True)
 
-sanitation_data.to_csv(r'./new_data/sanitation_data.csv')
+sanitation_data.to_csv(r'../data/processed/sanitation_data.csv')
 
-IDH_data.to_csv(r'./new_data/IDH_data.csv')
+IDH_data.to_csv(r'../data/processed/IDH_data.csv')
 
-mortality_data.to_csv(r'./new_data/mortality_data.csv')
+mortality_data.to_csv(r'../data/processed/mortality_data.csv')
 
-country_data.to_csv(r'./new_data/countries_data.csv')
+country_data.to_csv(r'../data/processed/countries_data.csv')
